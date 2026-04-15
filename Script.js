51b556fbf11a418f785b495eb7966b417c407c3e
zypack572-design <zypack572@gmail.com>
2026-04-15 17:51:24 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const db = new sqlite3.Database('./users.db');
+
+// Create a table to store users
+db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname)));
+
+// Route to handle signup/login data storage
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+    
+    const query = `INSERT INTO users (username, password) VALUES (?, ?)`;
+    db.run(query, [username, password], (err) => {
+        if (err) {
+            return res.status(500).send("Error saving to database.");
+        }
+        res.send("Registration successful! Data stored in users.db.");
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
+});
